ce561256a4715edcde86ef6cbbab301fac9ef601
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2024-07-17 02:25:57 -0500

    update-styles

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -11,16 +11,16 @@ const Home = () => {
         <div className='home_image_container'>
           <img src={Corona} alt='pic'/></div>
     <div className='welcome'>
-        <span>Welcome</span>
+     Welcome
     </div>
 
-     <div className = " btn-container">
+     <div className = "btn-container">
       <Link to='/form'
       className='btn'>
       Book Instantly
       </Link>
       </div>
-       <p>Luxury Rides For Less</p>
+       <p>Your Limo Service</p>
     </div>
   );
 }
